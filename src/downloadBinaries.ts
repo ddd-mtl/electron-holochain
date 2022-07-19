b50628ff9ee60d079e024c0d1180fb768adedd8c
damien <doute.d@gmail.com>
2022-07-18 21:25:07 -0400

    use runner 0.0.38-136

--- src/downloadBinaries.ts
+++ src/downloadBinaries.ts
@@ -65,7 +65,7 @@ async function downloadBinaries(tag: string) {
   try {
     // current holochain-runner release version
     // version-bump
-    const holochainRunnerTag = 'v0.0.38b'
+    const holochainRunnerTag = 'v0.0.38-136'
     await downloadBinaries(holochainRunnerTag)
   } catch (e) {
     console.log(e)
